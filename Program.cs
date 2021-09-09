3000da9c22905032e00143bc9bb7cb03f8392ee0
Egor <egorluka365@gmail.com>
2021-09-09 15:34:15 +0300

    -подправил код (вынес заполнение текста и нажатие кнопок в отдельные методы для лучшей читаемости кода)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace UIAutomationTaskSolution
         {
             UserProfile user = new UserProfile();
             user.Login = "Egor@email.com";
-            user.Password = "!QAZ";
-            
+            user.Password = "12";
+
             AutomationElement autElement =
                 AutomationElement.RootElement.FindFirst(TreeScope.Descendants,
                     new PropertyCondition(AutomationElement.NameProperty, "MainWindow"));
@@ -17,32 +17,37 @@ namespace UIAutomationTaskSolution
             AutomationElement buttonControl = autElement.FindFirst(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.NameProperty, "Log In"));
 
-            InvokePattern clickOnButton = buttonControl.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
-            
-            if (clickOnButton != null)
-            {
-                clickOnButton.Invoke();
+            ClickOnButton(buttonControl);
 
-                autElement = AutomationElement.RootElement.FindFirst(TreeScope.Descendants,
-                    new PropertyCondition(AutomationElement.ClassNameProperty, "#32770"));
+            autElement = AutomationElement.RootElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.ClassNameProperty, "#32770"));
 
-                AutomationElement textFieldControl = autElement.FindFirst(TreeScope.Descendants,
-                    new PropertyCondition(AutomationElement.AutomationIdProperty, "1003"));
+            AutomationElement textFieldControl = autElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "1003"));
 
-                ValuePattern vp = textFieldControl.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
-                vp.SetValue(user.Login);
+            SetText(textFieldControl, user.Login);
 
-                textFieldControl = autElement.FindFirst(TreeScope.Descendants,
-                    new PropertyCondition(AutomationElement.AutomationIdProperty, "1005"));
-                
-                vp = textFieldControl.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
-                vp.SetValue(user.Password);
+            textFieldControl = autElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "1005"));
 
-                buttonControl = autElement.FindFirst(TreeScope.Descendants,
-                    new PropertyCondition(AutomationElement.NameProperty, "ОК"));
-                clickOnButton = buttonControl.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
-                clickOnButton.Invoke();
-            }
+            SetText(textFieldControl, user.Password);
+
+            buttonControl = autElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.NameProperty, "ОК"));
+
+            ClickOnButton(buttonControl);
+        }
+
+        public static void SetText(AutomationElement textField, string text) // заполнение полей
+        {
+            ValuePattern vp = textField.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            if (vp != null) vp.SetValue(text);
+        }
+
+        public static void ClickOnButton(AutomationElement button) // нажатие на кнопку
+        {
+            InvokePattern clickOnButton = button.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            if (clickOnButton != null) clickOnButton.Invoke();
         }
     }
 }
